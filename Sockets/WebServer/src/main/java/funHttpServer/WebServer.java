48ba24024d3744b7821cb23c72a24eee29243f42
Tyler <93885793+tvterf@users.noreply.github.com>
2022-01-22 21:31:17 -0700

    Update WebServer.java

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -204,6 +204,14 @@ class WebServer {
           // extract required fields from parameters
           Integer num1 = Integer.parseInt(query_pairs.get("num1"));
           Integer num2 = Integer.parseInt(query_pairs.get("num2"));
+           
+            if(num1 == null || num2 == null){
+            builder.append("HTTP/1.1 400 Bad Request\n");
+            builder.append("Content-Type: text/html; charset=utf-8\n");
+            builder.append("\n");
+            builder.append("Must enter 2 Integers" );
+          }
+          else {
 
           // do math
           Integer result = num1 * num2;
@@ -213,6 +221,7 @@ class WebServer {
           builder.append("Content-Type: text/html; charset=utf-8\n");
           builder.append("\n");
           builder.append("Result is: " + result);
+        }
 
           // TODO: Include error handling here with a correct error code and
           // a response that makes sense
