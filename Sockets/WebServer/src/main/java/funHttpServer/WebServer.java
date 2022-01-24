3fd6b3fb601858837dc218b60e83eadd4432eee5
Tyler <93885793+tvterf@users.noreply.github.com>
2022-01-24 14:40:35 -0700

    Update WebServer.java

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -339,9 +339,10 @@ class WebServer {
           }catch(StringIndexOutOfBoundsException siobe){
           empty = true;
           }
-            String from = fetchURL("https://translate.google.com/" + query_pairs.get("sl") + query_pairs.get("tl") + query_pairs.get("text") + query_pairs.get("op") ); 
-           builder.append(from);
-           
+           String t1 = query_pairs.get("word1");
+           String t2 = query_pairs.get("word2");
+           String result = fetchURL("https://translate.google.com/?sl=en&tl=es&text=" + t1 + "&op=translate");
+           builder.append(result);
         }
            //else if(){
            
