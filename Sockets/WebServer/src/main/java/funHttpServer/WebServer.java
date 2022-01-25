a9aebf3d90c06edbe2705c1919008b328821abee
Tyler <93885793+tvterf@users.noreply.github.com>
2022-01-24 18:19:46 -0700

    Update WebServer.java

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -357,7 +357,7 @@ class WebServer {
               second = "DEFAULT";
            }
            
-           if(error == true){
+           if(empty == true){
           builder.append("HTTP/1.1 400 Bad Request\n");
           builder.append("Content-Type: text/html; charset=utf-8\n");
           builder.append("\n");
