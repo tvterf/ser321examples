4915cc65b463f627c0e1a104cd8d92c650746424
Tyler <93885793+tvterf@users.noreply.github.com>
2022-01-24 19:23:00 -0700

    Update WebServer.java

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -452,7 +452,7 @@ class WebServer {
           builder.append("\n"); 
                  double percent = iRate/100;
                  double num = 1 + percent;
-                 double factor = Pow(num, periods);
+                 double factor = math.pow(num, periods);
                  double result = pVal*factor;
                  builder.append("The future value of " + pVal + " with an interest rate of " + iRate + " for " + periods + " is:" + result);         
               }  
